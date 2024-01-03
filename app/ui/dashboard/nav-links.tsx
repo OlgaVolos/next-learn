894e2f321f4888c2a34dbebc69284c70e41b5f22
OlgaVolos <72765623+OlgaVolos@users.noreply.github.com>
2024-01-03 17:05:10 +0200

    pathname innav-links

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -22,13 +22,12 @@ const links = [
 ];
 
 export default function NavLinks() {
-  const pathname = useParams();
+ const pathname : any = useParams();
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
 
-        // @ts-ignore
         return (
           <Link
             key={link.name}
@@ -38,7 +37,7 @@ export default function NavLinks() {
                 'gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium ' +
                 'hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
                 {
-                  'bg-sky-100 text-blue-600': pathname === link.href,
+                  'bg-sky-100 text-blue-600': pathname == link.href,
                 },
             )}
           >
